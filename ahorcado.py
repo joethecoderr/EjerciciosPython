5cd822558fb060091fac10c9b93d0398f01bb4ae
joethecoderr <joel.e.gaspar@gmail.com>
2019-12-14 14:50:21 -0700

    Ahorcado

---
+++ ahorcado.py
@@ -0,0 +1,117 @@
+import random
+
+IMAGES = ['''
+
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|   |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+   /    |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+   / \  |
+        =========''', '''
+''']
+
+WORDS = [
+    'lavadora',
+    'secadora',
+    'sofa',
+    'gobierno',
+    'diputado',
+    'democracia',
+    'computadora',
+    'teclado',
+    'AMLO',
+
+]
+
+def random_word():
+    idx = random.randint(0, len(WORDS) - 1)
+    return WORDS[idx]
+
+def display_board(hidden_word, tries):
+    print(IMAGES[tries])
+    print('')
+    print(hidden_word)
+    print('-------*-------------*-------------*-----------*')
+
+def run():
+    word = random_word()
+    hidden_word =  ['-'] * len(word)
+    tries = 0
+
+    while True:
+        display_board(hidden_word, tries)
+        curr_letter = str(input('Escoge una letra: '))
+
+        letter_idx = []
+        for i in range(len(word)):
+            if word[i] == curr_letter:
+                letter_idx.append(i)
+                
+        if(len(letter_idx) == 0):
+            tries += 1
+        else:
+            for i in letter_idx:
+                hidden_word[i] = curr_letter
+
+            letter_idx = []
+
+if __name__ == '__main__':
+    print('BIENVENIDOS AL JUEGO DE AHORCADOS')
+    run()
